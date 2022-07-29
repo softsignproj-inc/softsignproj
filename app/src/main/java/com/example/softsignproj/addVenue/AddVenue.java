8db9c61b6133ce0a616bdce06396e57532ce1ae4
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-07-29 17:21:39 -0400

    created addVenue package

---
+++ app/src/main/java/com/example/softsignproj/addVenue/AddVenue.java
@@ -0,0 +1,14 @@
+package com.example.softsignproj.addVenue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddVenue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_add_venue); commented this out to get rid of error
+    }
+}
