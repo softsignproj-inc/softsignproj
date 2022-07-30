e2e3ceeffc7499c71a27cc51efe515c69d29308d
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-07-29 20:13:19 -0400

    Edited layout

--- app/src/main/java/com/example/softsignproj/addVenue/AddVenue.java
+++ app/src/main/java/com/example/softsignproj/addVenue/AddVenue.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.softsignproj.R;
+
+
 public class AddVenue extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_add_venue); commented this out to get rid of error
+        setContentView(R.layout.activity_add_venue);// commented this out to get rid of error
     }
 }
