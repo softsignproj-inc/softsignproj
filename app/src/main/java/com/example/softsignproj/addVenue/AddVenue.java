b901278d0f330ed344cc764fbf1fb85f76ee6e3a
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-07-31 00:04:33 -0400

    Added id to textView

--- app/src/main/java/com/example/softsignproj/addVenue/AddVenue.java
+++ app/src/main/java/com/example/softsignproj/addVenue/AddVenue.java
@@ -1,12 +1,17 @@
 package com.example.softsignproj.addVenue;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.softsignproj.FireBase;
 import com.example.softsignproj.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class AddVenue extends AppCompatActivity {
@@ -15,6 +20,7 @@ public class AddVenue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_venue);// commented this out to get rid of error
+        FireBase f = new FireBase();
     }
 
     public void onEnter(View view){
